3a6898342c3a807aca67d7bc4fe6751daeab8998
2hmedy2sser <ahkdyasr269@gmail.com>
2025-01-10 20:33:04 +0200

    firstcommit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  imgSrc:string=`/photo/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png`
+
+}
---
+++ src/app/portofolio/portofolio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portofolio',
+  imports: [],
+  templateUrl: './portofolio.component.html',
+  styleUrl: './portofolio.component.css'
+})
+export class PortofolioComponent {
+  imgSrc:string=`/photo/poert1.png`
+   imgSrc1:string=`/photo/port2.png`
+    imgSrc2:string=`/photo/port3.png`
+}
